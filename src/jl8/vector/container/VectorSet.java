98205afc77e671be8bfd7e54c77ad25177bc7a1c
Daria Struts <daryastruts@gmail.com>
2015-11-09 18:04:39 +0200

    VectorSet method add updated

--- src/jl8/vector/container/VectorSet.java
+++ src/jl8/vector/container/VectorSet.java
@@ -10,13 +10,6 @@ public class VectorSet extends VectorCollection  implements Set {
 
 
     public VectorSet(Vector[] inArr) {
-      /*  Vector[] newVectorArr = new Vector[0];
-        VectorCollection vectorCollection = new VectorCollection(newVectorArr);
-        for (int i = 0; i < inArr.length; i++) {
-            if (!(vectorCollection.contains(inArr[i]))){
-                vectorCollection.add(inArr[i]);
-            }
-        }*/
         super(inArr);
     }
 
@@ -25,16 +18,8 @@ public class VectorSet extends VectorCollection  implements Set {
         if (contains(o)){
             return false;
         }
-        if (o instanceof Vector) {
-            Vector[] tempArray = new Vector[arr.length];
-            System.arraycopy(arr, 0, tempArray, 0, arr.length);
-            arr = new Vector[arr.length + 1];
-            System.arraycopy(tempArray, 0, arr, 0, arr.length-1);
-            arr[arr.length-1] = (Vector)o;
-            return true;
-        } else {
-            throw new ClassCastException();
-        }
+        super.add(o);
+        return true;
     }
 
 }
